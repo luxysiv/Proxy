b1991882130d3483f9b418574dc607f69e667499
luxysiv <46205571+luxysiv@users.noreply.github.com>
2020-03-22 07:56:00 +0700

    update PAC.js

--- PAC.js
+++ PAC.js
@@ -33,6 +33,9 @@ var good_da_host_exceptions_exact_flag = 0 > 0 ? true : false;  // test for non-
 
 // rules:
 var bad_da_host_JSON = { "nastydollars.com": null,
+"adtima-media.zadn.vn":null,
+"log.sdk.zadn.vn":null,
+"e.crashlytics.com":null,
 "appbaqend.com":null,
 "applovin.com":null,
 "criteo com":null,
